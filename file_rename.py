a5af52e53faa6fdb88e6419bf3c34464d6f11c6b
christopherdsmd <chrisdesmond841@gmail.com>
2023-12-29 13:00:36 -0500

    added comments

--- file_rename.py
+++ file_rename.py
@@ -1,10 +1,13 @@
 import os
 os.getcwd()
 
+#renames files inside directory
 img_folder = r'C:\Users\chris\OneDrive\Desktop\Personal Coding Projects\Habbit Tracker and ToDo List\habbit\src\frog_photos'
 
+#gets count of images/files inside a folder
 img_name = os.listdir(img_folder)
 print(len(img_name), " images in the folder")
 
+#loop to renam all files with i - itterate count
 for i, filename in enumerate(os.listdir(img_folder)):
  os.rename(img_folder + "/" + filename, "frog_" + str(i) + ".png")
